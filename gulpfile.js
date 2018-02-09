c4b247cd07b21d9ddf002edab52de66c494786c8
yuchin <bow1130bow@gmail.com>
2018-02-09 18:22:28 +0800

    test

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+gulp.task('sass', function () {
+    //來源
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        //目的地
+        .pipe(gulp.dest('css/'));
+});
+
+
+gulp.task('default', ['sass'], function () {
+
+    browserSync.init({
+        server: {
+            //根目錄
+            baseDir: "./",
+            index: "tween.html"
+            // index: "id.html"
+        }
+    });
+
+    gulp.watch(["sass/*.scss", "sass/**/*.scss"], ['sass']).on('change', reload);
+    gulp.watch("*.html").on('change', reload);
+    gulp.watch("js/*.js").on('change', reload);
+    // gulp.watch("images/*").on('change', reload);
+});
