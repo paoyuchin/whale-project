9733f388d4d2034e095f434c8cd8ee6a394f5c7f
yuchin <bow1130bow@gmail.com>
2018-03-06 10:21:15 +0800

    before first time check!

---
+++ menu.php
@@ -0,0 +1,90 @@
+<!-- Start header -->
+<header class="header">
+	<nav class="navbar">
+		<div class="container">
+			<div class="row ">
+				<figure class="navbar-logo">
+					<a href="#">
+						<img src="images/logo/logo.png" alt="logo">
+					</a>
+				</figure>
+				<a href="#overlay" class="menu-toggle">
+					<span></span>
+					<span></span>
+					<span></span>
+				</a>
+				<div id="overlay" class="navbar-menu popup-nav">
+					<ul>
+						<li>
+							<a href="#" class="menu-item" id="about">關於我們</a>
+							<ul class="dropdown">
+								<li>
+									<a href="#">緣起</a>
+								</li>
+								<li>
+									<a href="#">里程碑</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="menu-item" id="event">活動招募</a>
+							<ul class="dropdown">
+								<li>
+									<a href="#">護鯨活動</a>
+								</li>
+								<li>
+									<a href="#">宣傳講座</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#" class="menu-item">募資捐款</a>
+						</li>
+						<li>
+							<a href="#" class="menu-item">連署活動</a>
+						</li>
+						<li>
+							<a href="#" class="menu-item">鯨典百科</a>
+						</li>
+						<li>
+							<a href="#" class="menu-item">會員</a>
+						</li>
+					</ul>
+					<a href="#!" class="popup-close">
+						<span></span>
+						<span></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</header>
+<!-- End header -->
+
+<!-- Start main -->
+<section class="event-main">
+
+</section>
+<!-- End main -->
+
+<!-- Custom navMenu dropdown -->
+<ul class="navMenu_dropdown" id="dropdownAbout">
+	<li>
+		<a href="#">緣起</a>
+	</li>
+	<li>
+		<a href="#">里程碑</a>
+	</li>
+</ul>
+
+<ul class="navMenu_dropdown" id="dropdownEvent">
+	<li>
+		<a href="#">護鯨活動</a>
+	</li>
+	<li>
+		<a href="#">宣傳講座</a>
+	</li>
+</ul>
+<!-- js -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/custom.js"></script>
